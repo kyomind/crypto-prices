3ec29e53e94fca1b9fff313c42ee962f1c64d012
kyo <odinxp@gmail.com>
2022-03-17 02:04:42 +0800

    feat: 新增以python config儲存指定coin ids

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import csv
 import os
+from configparser import ConfigParser
 from datetime import datetime
 
 from pycoingecko import CoinGeckoAPI
 
-client = CoinGeckoAPI()
-btc = client.get_price(ids='bitcoin', vs_currencies='usd')
-print(btc)
+config = ConfigParser()
+config.read('config.ini')
+coin_gecko = CoinGeckoAPI()
 
 
 def store_coin_list():
-    coin_list = client.get_coins_list()
+    coin_list = coin_gecko.get_coins_list()
     with open('coin_list.csv', 'w', newline='') as csv_file:
         fieldnames = ['id', 'symbol', 'name']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -18,7 +19,16 @@ def store_coin_list():
         writer.writerows(coin_list)
 
 
+def get_my_coin_ids():
+    coin_ids = config['my_coin_list']['coin_ids']
+    return coin_ids
+
+
 if __name__ == "__main__":
     # 檔案不存在時建立
     if not os.path.isfile('coin_list.csv'):
         store_coin_list()
+
+    coin_ids = get_my_coin_ids()
+    res = coin_gecko.get_price(ids=coin_ids, vs_currencies='usd')
+    print(res)
