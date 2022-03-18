2c2c96e3309cceddb394299bec4b49992990ff06
kyo <odinxp@gmail.com>
2022-03-18 11:59:43 +0800

    feat: 完成基本功能

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import csv
 import os
 from configparser import ConfigParser
-from datetime import datetime
 
 import gspread
 from pycoingecko import CoinGeckoAPI
 
-SHEET_URL = 'https://docs.google.com/spreadsheets/d/1WSZ8ckqIBPQA5V4FbUnfpisRhusd_WIvYGpgzKdCwak/'
-
 config = ConfigParser()
 config.read('config.ini')
 coin_client = CoinGeckoAPI()
+
+
+def create_id_symbol_mapping():
+    with open('coins.csv') as csv_file:
+        coins = csv.DictReader(csv_file)
+        return {coin['id']: coin['symbol'].upper() for coin in coins}
 
 
 def get_worksheet():
@@ -18,28 +21,38 @@ def get_worksheet():
     return sheet_client.open("Crypto Prices").sheet1
 
 
-
-def store_coin_list():
-    coin_list = coin_client.get_coins_list()
-    with open('coin_list.csv', 'w', newline='') as csv_file:
+def store_coins_csv():
+    coins = coin_client.get_coins_list()
+    with open('coins.csv', 'w', newline='') as csv_file:
         fieldnames = ['id', 'symbol', 'name']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
-        writer.writerows(coin_list)
+        writer.writerows(coins)
 
 
-def get_my_coin_ids():
-    coin_ids = config['my_coin_list']['coin_ids']
+def get_my_coin_ids() -> str:
+    coin_ids: str = config['my_coins']['coin_ids']
+    coin_ids = coin_ids.replace(' ', '').replace('\n', '')
     return coin_ids
+
+
+def convert_api_response_to_sheet_rows(api_response: dict) -> list:
+    id_to_symbol = create_id_symbol_mapping()
+    sheet_rows = []
+    for coin_id, coin_price in api_response.items():
+        row = [id_to_symbol[coin_id], coin_price['usd']]
+        sheet_rows.append(row)
+    return sheet_rows
 
 
 if __name__ == "__main__":
     # 檔案不存在時建立
-    if not os.path.isfile('coin_list.csv'):
-        store_coin_list()
+    if not os.path.isfile('coins.csv'):
+        store_coins_csv()
 
-    coin_ids = get_my_coin_ids()
-    res = coin_client.get_price(ids=coin_ids, vs_currencies='usd')
-    print(res)
+    my_coin_ids = get_my_coin_ids()
+    respsone = coin_client.get_price(ids=my_coin_ids, vs_currencies='usd')
+    sheet_rows = convert_api_response_to_sheet_rows(respsone)
+
     sheet = get_worksheet()
-    sheet.update('A1:B2', [[1, 2], [3, 4]])
+    sheet.update(f'A2:B{len(sheet_rows)+1}', sheet_rows)
