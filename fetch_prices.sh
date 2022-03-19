ca9406818694ca7d8360507ae2a4f06ce2f6a3b8
kyo <odinxp@gmail.com>
2022-03-19 14:28:04 +0800

    fix: 修正執行檔內容

--- fetch_prices.sh
+++ fetch_prices.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 source /home/${USER}/code/crypto-prices/.venv/bin/activate
-python /home/${USER}/code/crypto-prices/main.py >> /home/${USER}/code/crypto-prices/crontab.log 2>&1
+python /home/${USER}/code/crypto-prices/main.py >> /home/${USER}/code/crypto-prices/python.log 2>&1
 deactivate
